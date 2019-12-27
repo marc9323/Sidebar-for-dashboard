a3dd00a0e9fb1acdf86f50360b6041d722020bc4
marcholman9323@gmail.com <marcholman9323@gmail.com>
2019-12-26 18:46:30 -0600

    menu complete

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ document.querySelector('.hamburger-menu').addEventListener('click', ()=> {
     // add class if doesn't exist, remove if it does, toggle it
     document.querySelector('.container').classList.toggle('change');
 });
+
+document.querySelector('.nav-list').addEventListener('click', (e)=> {
+    // parent element of the clicked element
+    const el = e.target.parentElement;
+   // console.log(el);
+   if(el.classList[0] == 'nav-link'){
+       el.nextElementSibling.classList.toggle('change');
+       el.classList.toggle('change');
+   }
+});
